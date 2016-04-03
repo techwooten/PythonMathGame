69b1f25c26a20822c84fea16a7ab42ae641e9053
TeamWoot <wooten.joseph@gmail.com>
2016-04-03 10:03:30 -0400

    changed variable person to name

--- main.py
+++ main.py
@@ -5,13 +5,13 @@
 from random import randint
 
 #use input function to get the players name
-person = input('Hello, may I have your name? ')
+name = input('Hello, may I have your name? ')
 
 #used to track score
 score = 0
 
 #greet the player
-print('Hello, ' + person + '\n')
+print('Hello, ' + name  + '\n')
 
 #this function uses the imported module to get a random number between 0 and 9
 def getNumber():
@@ -29,7 +29,7 @@ for _ in range(10):
     # here we validate that the answer provided was the correct answer and update the score
     if(int(answer) == correctAnswer):
         score = score +1
-        print('Good Job ' + person + ', your score is ' + str(score) + '\n')
+        print('Good Job ' + name  + ', your score is ' + str(score) + '\n')
     else:
         score = score -1
-        print('Oh no! ' + person + ', you got that answer wrong.  Your score is ' + str(score) + '\n')
+        print('Oh no! ' + name + ', you got that answer wrong.  Your score is ' + str(score) + '\n')
