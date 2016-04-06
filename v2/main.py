b130068abbe626119d69906d51328620093ecd8e
TeamWoot <wooten.joseph@gmail.com>
2016-04-05 20:56:39 -0400

    finished adding classes and validation

--- v2/main.py
+++ v2/main.py
@@ -27,8 +27,7 @@ class Player:
         return self.__score
 
 class Game:
-    __startTime = 0
-    __finishTime = 0
+    __startTime = 0.0
     __totalQuestions = 0
     __correctAnswers = 0
     
@@ -39,7 +38,7 @@ class Game:
         self.__finishTime = time.time()
 
     def iterationTime(self):
-        return str((self.__startTime - self.__finishTime)/60) + 'min'
+        return str("%.2f" % (time.time() - self.__startTime)) + ' seconds'
 
     #this function uses the imported module to get a random number between 0 and 9
     def getNumber(self,start, end):
@@ -58,7 +57,19 @@ class Game:
             firstNumber = self.getNumber(0,9) # getting the numbers to be added together
             secondNumber = self.getNumber(0,9)
             # below we ask the player for their answer
-            answer = input('What is the answer to ' + str(firstNumber) + ' + ' + str(secondNumber) + '? ')
+            question = 'What is the answer to ' + str(firstNumber) + ' + ' + str(secondNumber) + '? '
+            answer = 0
+            badCount = 0
+            while True:
+                try:
+                    if(badCount > 3):
+                        break
+                    answer = int(input(question))
+                    break
+                except ValueError:
+                    print('Opps, your answer needs to be a number \n')
+                    badCount += 1
+            
             # generate the correct answer
             correctAnswer = firstNumber + secondNumber
             
@@ -70,7 +81,6 @@ class Game:
             else:
                 player.updateScore(-1)
                 print('Oh no! ' + player.getName() + ', you got that answer wrong.  Your score is ' + str(player.getScore()) + '\n')
-                
 
 
 #use input function to get the players name
@@ -81,17 +91,23 @@ player = Player(name)
 #greet the player
 print('Hello, ' + player.getName()  + '\n')
 
-play = input('Would you like to play a game? (Yes/No)')
 game = Game()
-while(play == 'Yes'):
-    game.startIteration()
-    print('You have answered ' + str(game.getCorrectAnswers()) + ' right in ' + str(game.iterationTime())
-    play = input('Would you like to play a game? (Yes/No)')
 
-if play == 'No' :
-    print('Ok, maybe next time')
-else:
-    print('Please type either Yes or No')
+while True:
+    play = input('Would you like to play a game? (Yes/No)')
+    if play.lower() not in ('yes','no'):
+        print('Please type either yes or no')
+    else:
+        if play.lower() == 'yes':
+            game.startIteration()
+            print('You have answered ' + str(game.getCorrectAnswers()) + ' right in ' + str(game.iterationTime()))
+            play = input('Would you like to play a game? (Yes/No)')
+    
+        if play.lower() == 'no' :
+            print('Ok, maybe next time')
+            break
+                 
+
 
 
 
